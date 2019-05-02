78bae2b306a62d3e63ef3e23461aaadf760d6e20
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-03 00:06:25 +0530

    finishing styling app

--- src/App.js
+++ src/App.js
@@ -35,29 +35,33 @@ const App = () => {
   }
 
   return (
-    <>
-      <form onSubmit={handleSearch}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img src="https://icon.now.sh/react/c0c" alt="React Logo" className=" float-right h-12"/>
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-4">
         <input 
           type="text" 
           value={query}
           ref={searchInputRef}
           onChange={event => setQuery(event.target.value)}
+          className="border p-1 rounded"
         />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>Clear</button>
+        <button type="submit" className="bg-orange rounded m-1 p-1">Search</button>
+        <button type="button" className="bg-teal text-white p-1 rounded" onClick={handleClearSearch}>Clear</button>
       </form>
-      { loading ? <div>Loading...</div> : <ul>
+      { loading ? <div className="font-bold text-orange-dark">Loading...</div> : 
+      <ul className="list-reset leading-normal">
         {results.map(result => (
           <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
+            <a href={result.url} className="text-indigo-dark hover:text-indigo-darkest">{result.title}</a>
           </li>
         ))}
       </ul>}
 
       { error && (
-        <div>{error.message}</div>
+        <div className="text-red font-bold">{error.message}</div>
       )}
-    </>
+    </div>
   );
 };
 
