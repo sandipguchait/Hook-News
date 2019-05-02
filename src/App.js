a4e4523790178257ba592c62ca159f24e98d76cc
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-02 23:52:41 +0530

    Finishing sunctionality of app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 
-const App = (props) => {
+const App = () => {
+
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('technology');
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
+  const searchInputRef = useRef();
+
+  useEffect(() => {
+        getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err)
+    }
+    setLoading(false);
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getData();  
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  }
+
   return (
-    <div>App</div>
+    <>
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          value={query}
+          ref={searchInputRef}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
+      { loading ? <div>Loading...</div> : <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>}
+
+      { error && (
+        <div>{error.message}</div>
+      )}
+    </>
   );
 };
 
